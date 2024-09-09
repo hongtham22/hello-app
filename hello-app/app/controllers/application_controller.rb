722a090458fda97ea1b908c08763097945a804d3
hongtham22 <hththam22@gmail.com>
2024-09-09 14:51:24 +0700

    chapter 2: a toy app

--- hello-app/app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-    def hello
-        render html: "¡Hola, mundo!”"
-    end
-    def goodbye
-        render html: "goodbye, world!"
-    end
-end
--- hello-app/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  # root 'application#hello'
-  root 'application#goodbye'
-end
